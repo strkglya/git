8bd9ff8527febca424745784676ed2c31ebb0692
strkglya <alealehandra@mail.ru>
2023-12-17 17:49:04 +0300

    first commit

---
+++ git/gitApp.swift
@@ -0,0 +1,17 @@
+//
+//  gitApp.swift
+//  git
+//
+//  Created by Александра Среднева on 17.12.23.
+//
+
+import SwiftUI
+
+@main
+struct gitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
