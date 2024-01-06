7f533dd0e76b81694ddc53c0ce45c101c2eb7ddd
strkglya <alealehandra@mail.ru>
2024-01-06 23:29:59 +0300

    new proj

---
+++ hw6/hw6/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  hw6
+//
+//  Created by Александра Среднева on 6.01.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
